307657aaf63d7a1780aa47d570bf0aaa228ac6f6
Leonard0SSilva <s2leoj@gmail.com>
2026-05-03 19:06:40 -0300

    Import ads

--- LiveOps/Deploy/Core/LiveOps.DTO/Json/CrossPlatformTypeBinder.cs
+++ LiveOps/Deploy/Core/LiveOps.DTO/Json/CrossPlatformTypeBinder.cs
@@ -62,7 +62,6 @@ namespace LiveOps.DTO.Json
                 name.StartsWith("LiveOps.", StringComparison.Ordinal) ||
                 name.StartsWith("LiveOps.Modules.", StringComparison.Ordinal) ||
                 name.StartsWith("LiveOps.Modules", StringComparison.Ordinal) ||
-                name.StartsWith("Game.LiveOps.", StringComparison.Ordinal) ||
                 name.StartsWith("Unity.", StringComparison.Ordinal))
             {
                 return true;
